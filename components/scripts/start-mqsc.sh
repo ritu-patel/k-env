49c0fd09e029537e50b83876f5de858047779e72
Ritu Patel <ritu.patel@ibm.com>
2022-03-02 12:48:03 -0500

    k-env

--- components/scripts/start-mqsc.sh
+++ components/scripts/start-mqsc.sh
@@ -18,11 +18,3 @@ while true; do
    fi
 
 done
-
-cd ..
-ingress_secret_name=$(oc get ingresscontroller.operator default \
---namespace openshift-ingress-operator \
--o jsonpath='{.spec.defaultCertificate.name}')
-oc extract secret/$ingress_secret_name -n openshift-ingress
-oc create secret tls -n openshift-gitops argocd-tls --cert=tls.crt --key=tls.key --dry-run=client -o yaml | oc apply -f -
-cd -
